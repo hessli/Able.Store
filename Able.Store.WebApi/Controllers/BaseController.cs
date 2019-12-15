29020ca4acf445773bd6ea2e659605f37542787d
hessli <hnyxlmq@163.com>
2019-12-15 12:16:24 +0800

    xx

--- Able.Store.WebApi/Controllers/BaseController.cs
+++ Able.Store.WebApi/Controllers/BaseController.cs
@@ -1,11 +1,16 @@
-﻿using Able.Store.IService;
+﻿using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace Able.Store.WebApi.Controllers
 {
     public  class BaseController : ApiController
     {
-     
+        protected override ResponseMessageResult ResponseMessage(HttpResponseMessage response)
+        {
+            var p= response;
+            return base.ResponseMessage(response);
+        }
 
     }
 }
